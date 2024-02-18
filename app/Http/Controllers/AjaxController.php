44c4db9b78e7852296dca1f1bb12b6ddd576cc6d
Andrew Lerma <andrewramonlerma@gmail.com>
2024-02-18 18:18:23 -0500

    initial app commit

---
+++ app/Http/Controllers/AjaxController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AjaxController extends Controller
+{
+    public function handleRequest($route)
+    {
+        // Implement logic for handling different AJAX routes
+        $autoTransitionLength = 0;
+        $autoTransitionDestination = "";
+
+        switch ($route) {
+            case 'start':
+                $elements = "<h1 class='animate-text amarante-regular' data-key-param='{\"l\":\"leave-cabin\"}'>Press L to Leave</h1>";
+                $background = "./images/cabin-interior.webp";
+                break;
+            case 'leave-cabin':
+                $elements = "";
+                $background = "./images/off-to-fish.webp";
+                $autoTransitionLength = 2;
+                $autoTransitionDestination = "interior-fish";
+                break;
+            case 'interior-fish':
+                $elements = "<h1 class='animate-text amarante-regular' data-key-param='{\"f\":\"attempt-fish\"}'>Press F to Fish</h1>";
+                $background = "./images/fishing-view.webp";
+                break;
+            case 'attempt-fish':
+                //run random fish attempt, include livewell storage $this->attemptFish($livewell)
+                $elements = "
+                    <h1 class='animate-text amarante-regular' data-key-param='{\"r\":\"release-fish\"}'>Press R to Release</h1>
+                    <h1 class='animate-text amarante-regular' data-key-param='{\"s\":\"store-fish\"}'>Press S to Store in Livewell</h1>
+                ";
+                $background = "./images/fishing-view.webp";
+                break;
+            default:
+                // Handle unknown routes
+                $elements = '<h1>Unknown Route</h1>';
+                $background = "./images/pftf-cabin.gif";
+        }
+
+        return response()->json([
+            'elements' => $elements,
+            'background' => $background,
+            'autoTransition' => $autoTransitionLength,
+            'autoTransitionDestination' => $autoTransitionDestination
+        ]);
+    }
+
+    public function fishAttempt()
+    {
+        //generate random number between one and one thousand
+        $randomNumber = mt_rand(1, 1000);
+        //pull fish assigned range
+        return $randomNumber;
+
+    }
+
+    public function storeFish($livewell)
+    {
+        if ($livewell <= 7) {
+            //store fish only when space allows
+            //assign fish space in livewell
+        }
+    }
+}
---
+++ public/js/game.js
@@ -0,0 +1,65 @@
+console.log('loaded');
+$(document).keypress(function(event) {
+    //access the pressed key using event.key
+    var pressedKey = event.key;
+    console.log(pressedKey+" pressed");
+
+    //get data-attr of every option on screen
+    var availableElements = document.querySelectorAll('[data-key-param]');
+
+    //iterate through the elements and find the matching key
+    availableElements.forEach(function(element) {
+        //parse the data-key-param attribute value as JSON
+        var keyParam = JSON.parse(element.getAttribute('data-key-param'));
+
+        //check if the pressed key matches the key in the attribute
+        if (keyParam[pressedKey]) {
+            //start transition
+            console.log(keyParam[pressedKey]+" attached");
+            //ajax request to retrieve new page and replace current
+            $.ajax({
+                url: '/ajax/' + keyParam[pressedKey],
+                method: 'GET',
+                success: function(response) {
+                    console.log(response);
+                    //replace the body content with the new content
+                    $('#game-container').fadeOut(2000, function() {
+                        $('#game-container').html(response.elements).fadeIn();
+                    });
+                    bodyTransition(response.background);
+                    if (response.autoTransitionDestination) {
+                        setTimeout(() => {
+                            $.ajax({
+                                url: '/ajax/' + response.autoTransitionDestination,
+                                method: 'GET',
+                                success:function(response) {
+                                    $('#game-container').fadeOut(2000, function() {
+                                        $('#game-container').html(response.elements).fadeIn();
+                                    });
+                                    bodyTransition(response.background);
+                                }
+                            })
+                        }, 4000);
+                    }
+                },
+                error: function(error) {
+                    console.error('Error fetching content:', error);
+                }
+            });
+        }
+    });
+});
+
+//get all elements with class "animate-text"
+var animateElements = document.querySelectorAll('.animate-text');
+
+//apply different animation delays to each element
+animateElements.forEach(function(element, index) {
+    element.style.animationDelay = index * 0.1 + 's';
+});
+
+function bodyTransition(background) {
+    $('#background-container').fadeOut(2000, function() {
+        $('#background-container').css("background-image", "url("+background+")").fadeIn("slow").fadeIn(2000);
+    });
+}
