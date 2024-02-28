de9a38196e5487440274953500f2a53837aebb81
Andrew Lerma <andrewramonlerma@gmail.com>
2024-02-27 21:16:54 -0500

    updates to text search game

--- public/js/survey.js
+++ public/js/survey.js
@@ -17,15 +17,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 return;
             }
 
-            const element = document.getElementById(input);
-            //word matched
-            if (element) {
-                message.innerText = element.textContent;
+            const searchedTerm = $('#inputField').val();
+            const element = document.querySelector('#responseField');
+            const responseField = document.querySelector('#responseField');
+            const isInsideResponseField = responseField.querySelector('#' + searchedTerm);
+
+            if (isInsideResponseField) {
+                console.log('found');
                 message.style.opacity = '1';
+                message.innerText = isInsideResponseField.textContent;
                 setTimeout(() => {
+                    console.log('found timeout');
                     message.style.opacity = '0';
                     var elementToRemove = document.getElementById(input);
                     elementToRemove.parentNode.removeChild(elementToRemove);
+                    $('#message').empty();
+                }, 2000);
+            } else {
+                console.log('not found');
+                $(inputField).css('border', '2px solid red !important');
+                setTimeout(() => {
+                    console.log('not found timeout');
+                    $(inputField).css('border', 'none');
                 }, 2000);
             }
 
--- resources/views/survey.blade.php
+++ resources/views/survey.blade.php
@@ -9,17 +9,36 @@
         <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh;">
             <div class="text-center">
                 <div id="game-container">
-                    <h1 class="animate-text amarante-regular" data-key-param='{"s":"start"}'>Type "done" when finished</h1>
+                    <h1 class="animate-text amarante-regular d-none">Type "done" when finished</h1>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div id="survey">
-                <input type="text" id="inputField" class="w-50 m-auto mb-3" autofocus>
-                <div id="message"></div>
-                <div id="apple" class="d-none">Apple</div>
-                <div id="banana" class="d-none">Banana</div>
-                <div id="cherry" class="d-none">Cherry</div>
+            <div id="survey" class="text-center">
+                <div class="row">
+                    <div id="responseContainer">
+                        <h3 id="message"></h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <input type="text" id="inputField" class="w-50 m-auto mb-3" style="border: 2px solid darkgrey" autofocus>
+                </div>
+                <div class="row">
+                    <div id="responseField">
+                        <div id="moutain" class="d-none">Mountain</div>
+                        <div id="lake" class="d-none">Lake</div>
+                        <div id="cave" class="d-none">Cave</div>
+                        <div id="trees" class="d-none">Trees</div>
+                        <div id="snow" class="d-none">Snow</div>
+                        <div id="sand" class="d-none">Sand</div>
+                        <div id="beach" class="d-none">Beach</div>
+                        <div id="rocks" class="d-none">Rocks</div>
+                        <div id="birds" class="d-none">Birds</div>
+                        <div id="clouds" class="d-none">Clouds</div>
+                        <div id="turtle" class="d-none">Turtle</div>
+                        <div id="dolphin" class="d-none">Dolphin</div>
+                    </div>
+                </div>
                 <!-- more elements here -->
             </div>
         </div>
